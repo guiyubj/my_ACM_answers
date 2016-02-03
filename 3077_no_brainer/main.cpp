b5e97fcfb6aafe73005b8d4873be0ee63e0e2e5a
Yu Gui <guiyubj@hotmail.com>
2016-02-03 12:44:38 -0500

    Update main.cpp

--- 3077_no_brainer/main.cpp
+++ 3077_no_brainer/main.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main(){
+	//get dataset number
 	short i_dataset_num;
 	cin >> i_dataset_num;
-
+	
+	//create a dataset that have position for twince the dataset number
 	short arr_dataset[i_dataset_num << 1];
-
+	
+	//start get input of data and at the same time solve problem
 	short i_brain_eat;
 	short i_brain_required;
 	for(short j=0; j<i_dataset_num; j++){
