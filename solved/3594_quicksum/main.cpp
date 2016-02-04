8a3c0e7aeb77126e08ed7ca13f3ae855c72c58e1
guiyubj <yuguicode@gmail.com>
2016-02-03 21:50:08 -0500

    AC

--- solved/3594_quicksum/main.cpp
+++ solved/3594_quicksum/main.cpp
@@ -4,19 +4,21 @@
 using namespace std;
 
 int main() {
+
 	//get line by line
 	string str_line;
-	while (getline(cin, str_line)) {
+	while (true) {
+		getline(cin, str_line);
+
 		if (str_line != "#") {
 			//start to sum line by char
 			int i_sum = 0;
-			for (int j = 0; j != str_line.size() - 1; j++) {
+			for (int j = 0; j != str_line.size(); j++) {
 				if (str_line[j] > 64 && str_line[j] < 91) {
 					i_sum += int(str_line[j] - 64) * (j + 1);
 				}
 			}
 
-			//print result
 			cout << i_sum << endl;
 		}
 		else {
